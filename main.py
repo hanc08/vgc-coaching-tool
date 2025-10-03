ed3dd8bfef98b1d0b1990d5e4cb74faf0bbdd0f1
darts11 <mysticmonster33@gmail.com>
2025-10-02 20:34:56 -0400

    fixed

--- main.py
+++ main.py
@@ -58,38 +58,6 @@ def data():
 def levels():
     return render_template('levels.html')
 
-@main.route('/level1')
-@login_required
-def level1():
-    path = 'level1.html'
-    levelName = 'levelexample'
-    mon1 = "Urshifu"
-    mon2 = "Miraidon"
-    target1 = "Weezing"
-    target2 = "Tatsugiri"
-    switch1 = "Whimsicott"
-    switch2 = "Incineroar"
-    canOne = "cant"
-    canTwo = "can"
-    tera = "can"
-    move1= "Surging Strikes"
-    typeM1 = "water"
-    move2 = "Close Combat"
-    typeM2 = "fighting"
-    move3 = "Aqua Jet"
-    typeM3 = "water"
-    move4 = "U-Turn"
-    typeM4 = "bug"
-    move5= "Electro Drift"
-    typeM5 = "electric"
-    move6 = "Draco Meteor"
-    typeM6 = "dragon"
-    move7 = "Volt Switch"
-    typeM7 = "electric"
-    move8 = "Dazzling Gleam"
-    typeM8 = "fairy"
-    replaySRC="https://replay.pokemonshowdown.com/gen9vgc2025regibo3-2410389774-lkdj5v33mo8n6jgngbqkwx9tsk9eht2pw?turn=10"
-    return render_template(path, levelTitle = levelName, pokemon1 = mon1, pokemon2 = mon2, targetOne = target1,targetTwo = target2, switchOne = switch1,switchTwo  = switch2, can1 = canOne, can2 = canTwo, tera1 = tera,tera2 = tera,moveOne = move1,typeMove1 = typeM1,moveTwo = move2,typeMove2 =typeM2,moveThree = move3,typeMove3 = typeM3,moveFour = move4,typeMove4 = typeM4,moveFive = move5,typeMove5 = typeM5,moveSix = move6,typeMove6 = typeM6,moveSeven = move7,typeMove7 = typeM7,moveEight = move8,typeMove8 = typeM8, replay=replaySRC)
 
 @main.route('/level1')
 @login_required
